1d18942333186cee41b9cffd5d5180b53861adc1
Ivan Storck <ivanoats@gmail.com>
2014-03-03 21:37:22 -0800

    renamed extension to .hbs

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ var app = express();
 // handlebars for express (from README)
 // https://github.com/ericf/express3-handlebars
 var exphbs  = require('express3-handlebars');
-app.engine('handlebars', exphbs(
+app.engine('hbs', exphbs(
   {
     defaultLayout: 'main',
     // I had to add this in addtion to the express3-handlebars README
-    //extname: 'hbs'
+    extname: '.hbs'
   }
 ));
-app.set('view engine', 'handlebars');
+//app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
 // instruct express to serve up static assets
 app.use(express.static('public'));
